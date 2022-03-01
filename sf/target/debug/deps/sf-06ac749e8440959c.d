97649f4ce341c78d3189520a4a505780fe373c58
Phydon <84594612+Phydon@users.noreply.github.com>
2022-03-01 22:03:31 +0100

    Add files via upload

---
+++ sf/target/debug/deps/sf-06ac749e8440959c.d
@@ -0,0 +1,5 @@
+/home/phydon/main/sf/target/debug/deps/sf-06ac749e8440959c: src/main.rs
+
+/home/phydon/main/sf/target/debug/deps/sf-06ac749e8440959c.d: src/main.rs
+
+src/main.rs:
---
+++ sf/target/debug/sf.d
@@ -0,0 +1,1 @@
+/home/phydon/main/sf/target/debug/sf: /home/phydon/main/sf/src/main.rs
