6fb834ff656e2ba26d93baf1396fe2e5af5b32a6
Phydon <84594612+Phydon@users.noreply.github.com>
2022-03-29 21:16:17 +0200

    Add files via upload

--- openfile/main.rs
+++ openfile/main.rs
@@ -9,7 +9,7 @@ fn main() {
 fn open_file(file: &str) {
     // change the tool to whatever is needed
     if cfg!(target_os = "windows") {
-        Command::new("notepad").arg(file).status().unwrap();
+        Command::new("cmd").args(["/C", file]).status().unwrap();
     } else {
         Command::new("vim").arg(file).status().unwrap();
     }
