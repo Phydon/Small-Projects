73ec1d5c482e00cb46de7bdc05c104ee98554678
Phydon <84594612+Phydon@users.noreply.github.com>
2022-04-04 20:33:30 +0200

    Add files via upload

--- openfile/main.rs
+++ openfile/main.rs
@@ -1,16 +1,19 @@
+use std::io;
 use std::process::Command;
 
 fn main() {
     let file_path: &str = "/home/phydon/main/test/testfile.txt";
 
-    open_file(file_path);
+    open_file(file_path).expect("Failed to execute command");
 }
 
-fn open_file(file: &str) {
+fn open_file(file: &str) -> io::Result<()> {
     // change the tool to whatever is needed
     if cfg!(target_os = "windows") {
-        Command::new("cmd").args(["/C", file]).status().unwrap();
+        Command::new("cmd").args(["/C", file]).status()?;
+        Ok(())
     } else {
-        Command::new("vim").arg(file).status().unwrap();
+        Command::new("vim").arg(file).status()?;
+        Ok(())
     }
 }
